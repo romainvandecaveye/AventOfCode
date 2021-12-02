46e57b46d893562d2fddc708fe7c4309895e3deb
Romain Vandecaveye <rvandecaveye@gmail.com>
2021-12-02 11:10:22 +0100

    feat (day2) : part 2

--- src/dive.test.ts
+++ src/dive.test.ts
@@ -5,14 +5,16 @@ console.log(diveData);
 class Coordinates {
   horizontal: number;
   depth: number;
+  aim: number;
 
-  constructor(horizontal: number, depth: number) {
+  constructor(horizontal: number, depth: number, aim: number) {
     this.horizontal = horizontal;
     this.depth = depth;
+    this.aim = aim;
   }
 
   multiply(): number {
-    return this.horizontal*this.depth;
+    return this.horizontal * this.depth;
   }
 }
 
@@ -32,75 +34,74 @@ function dive(orderStr: string): Coordinates {
   let order = new Order(orderStr);
   switch (order.direction) {
     case 'forward': {
-      return new Coordinates(order.unit, 0);
+      return new Coordinates(order.unit, 0, 0);
     }
     case 'down': {
-      return new Coordinates(0, order.unit);
+      return new Coordinates(0, 0, order.unit);
     }
     case 'up': {
-      return new Coordinates(0, -order.unit);
+      return new Coordinates(0, 0, -order.unit);
     }
     default: {
-      break;
+      return new Coordinates(1, 0, 0);
     }
   }
-  return new Coordinates(1, 0);
 }
 
 function diveFrom(orderStr: string, position: Coordinates): Coordinates {
   let delta = dive(orderStr);
-  let depth = position.depth + delta.depth;
+  let depth = position.depth + (position.aim * delta.horizontal);
   let horizontal = position.horizontal + delta.horizontal;
-  return new Coordinates(horizontal, depth);
+  let aim = position.aim + delta.aim;
+  return new Coordinates(horizontal, depth, aim);
 }
 
 function diveFromArray(orders: string[]): Coordinates {
-  let position = new Coordinates(0, 0);
+  let position = new Coordinates(0, 0, 0);
   for (let i = 0; i < orders.length; i++) {
     position = diveFrom(orders[i], position);
   }
-
   return position;
 }
 
 describe('simple dive', () => {
   it('should return [1,0] when it is forward 1', () => {
     let actual = dive('forward 1');
-    const expected = new Coordinates(1, 0);
+    const expected = new Coordinates(1, 0, 0);
     expect(actual).toStrictEqual(expected);
   });
-  it('should return [0,1] when it is down 1', () => {
+  it('should return [0,0,1] when it is down 1', () => {
     let actual = dive('down 1');
-    const expected = new Coordinates(0, 1);
+    const expected = new Coordinates(0, 0, 1);
     expect(actual).toStrictEqual(expected);
   });
-  it('should return [0,-1] when it is up 1', () => {
+  it('should return [0,0,-1] when it is up 1', () => {
     let actual = dive('up 1');
-    const expected = new Coordinates(0, -1);
+    const expected = new Coordinates(0, 0, -1);
     expect(actual).toStrictEqual(expected);
   });
 });
 
 describe('dive from position', () => {
-  it('should return [2,1] when it is forward 1 from position [1,1]', () => {
-    let actual = diveFrom('forward 1', new Coordinates(1, 1));
-    const expected = new Coordinates(2, 1);
+  it('should return [2,1,0] when it is forward 1 from position [1,1,0]', () => {
+    let actual = diveFrom('forward 1', new Coordinates(1, 1, 0));
+    const expected = new Coordinates(2, 1, 0);
     expect(actual).toStrictEqual(expected);
   });
-  it('should return [1,2] when it is down 1 from position [1,1]', () => {
-    let actual = diveFrom('down 1', new Coordinates(1, 1));
-    const expected = new Coordinates(1, 2);
+  it('should return [1,1,1] when it is down 1 from position [1,1,0]', () => {
+    let actual = diveFrom('down 1', new Coordinates(1, 1, 0));
+    const expected = new Coordinates(1, 1, 1);
     expect(actual).toStrictEqual(expected);
   });
-  it('should return [1,0] when it is up 1 from position [1,1]', () => {
-    let actual = diveFrom('up 1', new Coordinates(1, 1));
-    const expected = new Coordinates(1, 0);
+  it('should return [1,1,0] when it is up 1 from position [1,1,1]', () => {
+    let actual = diveFrom('up 1', new Coordinates(1, 1, 1));
+    const expected = new Coordinates(1, 1, 0);
     expect(actual).toStrictEqual(expected);
   });
 });
 
 describe('dive with array', () => {
-  it('should return [2,1] when it is forward 1 from example array [1,1]', () => {
+  it('should return 900 when it is forward 1 from example array ', () => {
     let exempleArray = [
       'forward 5',
       'down 5',
@@ -109,8 +110,8 @@ describe('dive with array', () => {
       'down 8',
       'forward 2'
     ];
-    let actual = diveFromArray(exempleArray);
-    const expected = new Coordinates(15, 10);
+    let actual = diveFromArray(exempleArray).multiply();
+    const expected = new Coordinates(15, 60,0).multiply();
     expect(actual).toStrictEqual(expected);
   });
 });
