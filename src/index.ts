db6921a308963020d0d1430acba5ebbb6341ff53
monsieurz <mrz1880@gmail.com>
2021-12-07 12:26:02 +0100

    ♻️ logTimestamptedResult

--- src/index.ts
+++ src/index.ts
@@ -10,71 +10,34 @@ import * as day3 from './Day3/diag';
 import * as day4 from './Day4/bingo';
 import { getNumberOfFishAfter } from './Day6/shoal';
 
-let startTime = new Date().getTime();
-const day1step1 = day1.getNumberOfIncreasedFromSimpleArrayOfNumber(sonarData);
-const endTimeDay1Step1 = Math.round(new Date().getTime() - startTime);
-console.log('{', endTimeDay1Step1, ' ms} Day1-step1 : ', day1step1);
-
-startTime = new Date().getTime();
-const day1step2 = day1.getNumberOfIncreasedFromDeepArrayOfNumber(sonarData);
-const endTimeDay1Step2 = Math.round(new Date().getTime() - startTime);
-console.log('{', endTimeDay1Step2, ' ms} Day1-step2 : ', day1step2);
-
-startTime = new Date().getTime();
-const day2step1 = day2.getFinalPositionAfterDive(diveData);
-const endTimeDay2Step1 = Math.round(new Date().getTime() - startTime);
-console.log('{', endTimeDay2Step1, ' ms} Day2-step1 : ', day2step1);
-
-startTime = new Date().getTime();
-const day2step2 = day2.getFinalPositionAfterDiveWithAim(diveData);
-const endTimeDay2Step2 = Math.round(new Date().getTime() - startTime);
-console.log('{', endTimeDay2Step2, ' ms} Day2-step2 : ', day2step2);
-
-startTime = new Date().getTime();
-const day3step1 = day3.calculatePowerConsumption(powerConsumptionData);
-const endTimeDay3Step1 = Math.round(new Date().getTime() - startTime);
-console.log('{', endTimeDay3Step1, ' ms} Day3-step1 : ', day3step1);
-
-startTime = new Date().getTime();
-const day3step2 = day3.calculateLifeSupportRating(powerConsumptionData);
-const endTimeDay3Step2 = Math.round(new Date().getTime() - startTime);
-console.log('{', endTimeDay3Step2, ' ms} Day3-step2 : ', day3step2);
-
-startTime = new Date().getTime();
-const day4step1 = day4.whoWinSooner(boards, draws).calculatePower();
-const endTimeDay4Step1 = Math.round(new Date().getTime() - startTime);
-console.log('{', endTimeDay4Step1, ' ms} Day4-step1 : ', day4step1);
-
-startTime = new Date().getTime();
-const day4step2 = day4.whoWinLast(boards, draws).calculatePower();
-const endTimeDay4Step2 = Math.round(new Date().getTime() - startTime);
-console.log('{', endTimeDay4Step2, ' ms} Day4-step2 : ', day4step2);
-
-// startTime = new Date().getTime();
-// const venturesWithoutDiag = parser.parseVentureAndFilterDiagonale(venturesData);
-// const endTimeDay5Step11 = Math.round(new Date().getTime() - startTime);
-// console.log('{', endTimeDay5Step11, ' ms} Day5-step1-parsing');
-//
-// startTime = new Date().getTime();
-// const day5step1 = day5.calculateOverlapping(venturesWithoutDiag);
-// const endTimeDay5Step12 = Math.round(new Date().getTime() - startTime);
-// console.log('{', endTimeDay5Step12, ' ms} Day5-step1', day5step1);
-
-// startTime = new Date().getTime();
-// const ventures = parser.parseVenture(venturesData);
-// const endTimeDay5Step21 = Math.round(new Date().getTime() - startTime);
-// console.log('{', endTimeDay5Step21, ' ms} Day5-step2-parsing');
-//
-// startTime = new Date().getTime();
-// const day5step2 = day5.calculateOverlapping(ventures);
-// const endTimeDay5Step22 = Math.round(new Date().getTime() - startTime);
-// console.log('{', endTimeDay5Step22, ' ms} Day5-step2', day5step2);
-
-startTime = new Date().getTime();
-const day6step1 = getNumberOfFishAfter(lanternFishData, 80);
-const endTimeDay6Step1 = Math.round(new Date().getTime() - startTime);
-console.log('{', endTimeDay6Step1, ' ms} Day6-step1 : ', day6step1);
-
-const day6step2 = getNumberOfFishAfter(lanternFishData, 256);
-const endTimeDay6Step2 = Math.round(new Date().getTime() - startTime);
-console.log('{', endTimeDay6Step2, ' ms} Day6-step1 : ', day6step2);
+function logTimestamptedResult(day: number, step: number, methodCall: number) {
+  const startTime = new Date().getTime();
+  const endTime = Math.round(new Date().getTime() - startTime);
+  console.log('{', endTime, ' ms} Day', day, 'step', step, ': ', methodCall);
+}
+logTimestamptedResult(
+  1,
+  1,
+  day1.getNumberOfIncreasedFromSimpleArrayOfNumber(sonarData)
+);
+logTimestamptedResult(
+  1,
+  2,
+  day1.getNumberOfIncreasedFromDeepArrayOfNumber(sonarData)
+);
+logTimestamptedResult(2, 1, day2.getFinalPositionAfterDive(diveData));
+logTimestamptedResult(2, 2, day2.getFinalPositionAfterDiveWithAim(diveData));
+logTimestamptedResult(
+  3,
+  1,
+  day3.calculatePowerConsumption(powerConsumptionData)
+);
+logTimestamptedResult(
+  3,
+  2,
+  day3.calculateLifeSupportRating(powerConsumptionData)
+);
+logTimestamptedResult(4, 1, day4.whoWinSooner(boards, draws).calculatePower());
+logTimestamptedResult(4, 2, day4.whoWinLast(boards, draws).calculatePower());
+logTimestamptedResult(6, 1, getNumberOfFishAfter(lanternFishData, 80));
+logTimestamptedResult(6, 2, getNumberOfFishAfter(lanternFishData, 256));
